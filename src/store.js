a4499b209331e477de790971b2432f6fd8acfe54
Nick Taylor <nick@iamdeveloper.com>
2020-05-01 14:13:17 -0400

    Added some comments.

--- src/store.js
+++ src/store.js
@@ -1,20 +1,43 @@
+/**
+ *
+ * @param {Function} reducer
+ * @param {object|number|string|boolean|Array} initialState
+ */
 export function createStore(reducer, initialState) {
   let state = initialState;
   let listeners = new Set();
 
+  /**
+   * Returns the current state of the Redux Store
+   */
   const getState = () => state;
 
+  /**
+   * Dispatches an action to update the Redux store and
+   * notifies subscribers, if any, of the state changes.
+   *
+   * @param {object} action
+   */
   const dispatch = (action) => {
     state = reducer(state, action);
 
+    // Notify all subscribers of state changes
     for (let [listener] of listeners.entries()) {
       listener(state);
     }
   };
 
+  /**
+   * Subscribes the given listener to the Redux store.
+   *
+   * @param {Function} listener
+   *
+   * @returns {Function} Returns an unsubscribe function
+   */
   const subscribe = (listener) => {
     listeners.add(listener);
 
+    // Subscription callback should the subscriber decide to unsubscribe.
     return () => {
       listeners.delete(listener);
     };
